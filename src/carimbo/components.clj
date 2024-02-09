2f2dff0093a9f4d8cd521ca6191de0390d1a51a8
Bruno do Nascimento Maciel <brunodonascimentomaciel@gmail.com>
2024-02-09 05:54:34 -0300

    fix components map

--- src/carimbo/components.clj
+++ src/carimbo/components.clj
@@ -4,7 +4,9 @@
             [common-clj.component.service :as component.service]
             [common-clj.component.datalevin :as component.datalevin]
             [carimbo.db.datalevin.config :as database.config]
-            [carimbo.customers :as customers])
+            [carimbo.customers :as customers]
+            [carimbo.diplomat.http-server :as diplomat.http-server]
+            [common-clj.component.routes :as component.routes])
   (:gen-class))
 
 (def system
@@ -12,8 +14,8 @@
     :config (component.config/new-config "resources/config.edn" :prod :edn)
     :datalevin (component/using (component.datalevin/new-datalevin database.config/schema) [:config])
     :customers (component/using (customers/new-customers) [:config :datalevin])
-    #_:routes #_(component/using (component.routes/new-routes diplomat.http-server/routes) [:config])
-    #_:service #_(component/using (component.service/new-service) [:routes :config])))
+    :routes (component.routes/new-routes diplomat.http-server/routes)
+    :service (component/using (component.service/new-service) [:routes :config])))
 
 (defn start-system! []
   (component/start system))
@@ -23,7 +25,7 @@
     :config (component.config/new-config "resources/config.example.edn" :test :edn)
     :datalevin (component/using (component.datalevin/new-datalevin database.config/schema) [:config])
     :customers (component/using (customers/new-customers) [:config :datalevin])
-    #_:routes #_(component/using (component.routes/new-routes diplomat.http-server/routes) [:config])
-    #_:service #_(component/using (component.service/new-service) [:routes :config])))
+    :routes (component.routes/new-routes diplomat.http-server/routes)
+    :service (component/using (component.service/new-service) [:routes :datalevin])))
 
 (def -main start-system!)
