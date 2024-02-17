5a36b2ffa94dcd4d559364171c6cdcae81f54d71
Bruno do Nascimento Maciel <brunodonascimentomaciel@gmail.com>
2024-02-17 10:23:20 -0300

    update project description

--- project.clj
+++ project.clj
@@ -1,9 +1,7 @@
 (defproject carimbo "0.1.0-SNAPSHOT"
 
-  :description "FIXME: write description"
-
-  :url "http://example.com/FIXME"
-
+  :description "My submission for the second edition of the 'Rinha de Backend' (Q1 2024)"
+  :url "https://github.com/macielti/carimbo"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url  "https://www.eclipse.org/legal/epl-2.0/"}
 
