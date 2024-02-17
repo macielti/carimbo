419aa72c342ff69068da7f541a39bf1a05e19420
Bruno do Nascimento Maciel <brunodonascimentomaciel@gmail.com>
2024-02-17 09:35:59 -0300

    fix postgresql component

--- src/carimbo/postgresql.clj
+++ src/carimbo/postgresql.clj
@@ -37,29 +37,46 @@
 
 (s/defn postgresql-for-unit-tests
   [schema-sql-path :- s/Str]
-  (let [postgresql-container (doto (PostgreSQLContainer. "postgres:15-alpine")
+  (let [postgresql-container (doto (PostgreSQLContainer. "postgres:16-alpine")
                                .start)
-        connection (-> (jdbc/get-connection {:dbtype  "postgresql"
-                                             :jdbcUrl (str (.getJdbcUrl postgresql-container) "&user=test&password=test")})
-                       (jdbc/with-options {:builder-fn rs/as-unqualified-maps}))]
+        db-connection (pool/make-datasource-spec
+                        {:classname         "org.postgresql.Driver"
+                         :subprotocol       "postgresql"
+                         :user              (.getUsername postgresql-container)
+                         :password          (.getPassword postgresql-container)
+                         :subname           (str "//" (.getHost postgresql-container)
+                                                 ":" (.getMappedPort postgresql-container
+                                                                     PostgreSQLContainer/POSTGRESQL_PORT)
+                                                 "/" (.getDatabaseName postgresql-container))
+                         :initial-pool-size 3
+                         :max-pool-size     14})]
 
-    (jdbc/execute! connection [(slurp schema-sql-path)])
+    (jdbc/execute! db-connection [(slurp schema-sql-path)])
 
-    {:database-connection  connection
+    {:database-connection  db-connection
      :postgresql-container postgresql-container}))
 
 (defrecord MockPostgreSQL [config]
   component/Lifecycle
   (start [component]
-    (let [postgresql-container (doto (PostgreSQLContainer. "postgres:15-alpine")
+    (let [postgresql-container (doto (PostgreSQLContainer. "postgres:16-alpine")
                                  .start)
-          postgresql-uri (str (.getJdbcUrl postgresql-container)
-                              "&user=test&password=test")
+          db-connection (pool/make-datasource-spec
+                          {:classname         "org.postgresql.Driver"
+                           :subprotocol       "postgresql"
+                           :user              (.getUsername postgresql-container)
+                           :password          (.getPassword postgresql-container)
+                           :subname           (str "//" (.getHost postgresql-container)
+                                                   ":" (.getMappedPort postgresql-container
+                                                                       PostgreSQLContainer/POSTGRESQL_PORT)
+                                                   "/" (.getDatabaseName postgresql-container))
+                           :initial-pool-size 3
+                           :max-pool-size     14})
           schema-sql (slurp "resources/schema.sql")]
 
-      (jdbc/execute! (get-connection postgresql-uri) [schema-sql])
+      (jdbc/execute! db-connection [schema-sql])
 
-      (assoc component :postgresql postgresql-uri
+      (assoc component :postgresql db-connection
                        :postgresql-container postgresql-container)))
 
   (stop [component]
